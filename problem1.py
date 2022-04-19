666b090dbdb881cc46ce0d3d1006ce73ebc8cd2f
mahamed amr ahmed mansi <36801941+midoamrm@users.noreply.github.com>
2022-04-19 21:39:40 +0300

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -1,28 +1,23 @@
-#big(o) n**2
-l_input=["1818005553531","31415926535","314159265385","81234567895","51234567898","581234567898"]
-def check_serial_number(l_input):
-    l=[]
-    serial_number_l=[]
-    num_of_problem=0
-    for i in range(0,len(l_input)):
-        flag=0
-        index=0
-        for j in range(0,len(l_input[i])):
-            if l_input[i][j]=="8":
-                index=j
-                flag=1
-                break
-        if flag==0:
-            serial_number_l.append(0)
-        else:
-           str=l_input[i][index:]
-           #print(str)
-           #print(len(str))
-           if len(str)>=11:
-             serial_number_l.append(1)
-           else:
-             serial_number_l.append(0)
-            
-    print(serial_number_l)
+n=int(input())
+l=[]
+countlist=[]
+num_of_problem=0
 
-check_serial_number(l_input)
+for i in range(0,n):
+    l.append(input())
+    #print(l[i])
+
+for k in range(0,n):
+    count=0
+    if l[k][0]=="1":
+        count=count+1
+    if l[k][2]=="1":
+        count=count+1
+    if l[k][4]=="1":
+        count=count+1
+    countlist.append(count)    
+for j in range(0,n):
+    temp=int(countlist[j])
+    if temp>=2:
+        num_of_problem=num_of_problem+1
+print( num_of_problem)
